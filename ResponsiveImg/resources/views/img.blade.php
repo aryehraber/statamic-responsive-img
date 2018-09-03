01da558869650742e81ce0f3536cfdb3bf1949b1
Aryeh Raber <aryeh.raber@gmail.com>
2018-09-04 00:10:06 +0200

    Tweak img view

--- ResponsiveImg/resources/views/img.blade.php
+++ ResponsiveImg/resources/views/img.blade.php
@@ -1,1 +1,1 @@
-<img {!! $attrs !!} srcset="{{ $srcset }}" onload="this.onload=null;this.sizes=Math.ceil(this.getBoundingClientRect().width/window.innerWidth*100)+'vw';" sizes="1px">
+<img {!! $attributes !!} src="{{ $image->getSrc() }}" srcset="{{ $image->getSrcset() }}" onload="this.onload=null;this.sizes=Math.ceil(this.getBoundingClientRect().width/window.innerWidth*100)+'vw';" sizes="1px" width="{{ $image->getWidth() }}">
